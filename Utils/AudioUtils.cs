a4d2ae35d11f184d668f8cfaa75d15d5b587df86
maks <borisovets.maksim@gmail.com>
2025-06-27 01:17:46 +0300

    add utils

---
+++ Utils/AudioUtils.cs
@@ -0,0 +1,15 @@
+﻿using NeoVibe.Core;
+
+namespace NeoVibe.Utils
+{
+    internal static class AudioUtils
+    {
+        internal static int GetFFTLength(int minLength)
+        {
+            int fftLength = 2;
+            while (fftLength < minLength && fftLength < Constants.MaxFFTLength)
+                fftLength *= 2;
+            return fftLength;
+        }
+    }
+}
