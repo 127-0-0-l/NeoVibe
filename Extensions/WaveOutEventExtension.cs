b2a69c99c1bea35f03dbca2d6adb2ce1b12fc001
maks <borisovets.maksim@gmail.com>
2025-06-27 00:04:27 +0300

    add waveOutEvent extension

---
+++ Extensions/WaveOutEventExtension.cs
@@ -0,0 +1,20 @@
+﻿using NAudio.Wave;
+
+namespace NeoVibe.Extensions
+{
+    internal static class WaveOutEventExtension
+    {
+        internal static bool IsInitialized(this WaveOutEvent waveOutEvent)
+        {
+            try
+            {
+                var state = waveOutEvent.PlaybackState;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
