f037c5196f8cfd1127d7057857eed51a2aabd230
maks <borisovets.maksim@gmail.com>
2025-06-26 23:27:13 +0300

    refactor

--- Core/VisualizerManager.cs
+++ Core/VisualizerManager.cs
@@ -7,22 +7,21 @@ namespace NeoVibe.Core
         private readonly Dictionary<string, IVisualizer> visualizers;
         private IVisualizer current;
 
-        public VisualizerManager(IEnumerable<IVisualizer> availableVisualizers)
+        internal VisualizerManager(IEnumerable<IVisualizer> availableVisualizers)
         {
             visualizers = availableVisualizers.ToDictionary(v => v.GetType().Name.ToLower());
             current = visualizers.Values.First();
         }
 
-        public void SetVisualizer(string name)
+        internal void SetVisualizer(string name)
         {
             if (visualizers.TryGetValue(name.ToLower(), out var visualizer))
             {
                 current = visualizer;
-                Console.WriteLine($"Визуализатор переключен на: {name}");
             }
             else
             {
-                Console.WriteLine($"Визуализатор '{name}' не найден.");
+                // show error
             }
         }
     }
